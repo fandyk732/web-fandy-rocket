70ae69077dcedfebc9f5878739f60a35b9a297f1
Fandyk Aziz Lukmana <fandyk732@guru.smk.belajar.id>
2026-08-15 23:27:33 +0700

    sitemap: add google site verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,8 +57,7 @@ export default function RootLayout({
         {children}
         <BackToTop />
 
-        <script type="module" async src="https://static.rocket.new/rocket-web.js?_cfg=https%3A%2F%2Ffandyaziz1931back.builtwithrocket.new&_be=https%3A%2F%2Fappanalytics.rocket.new&_v=0.1.20" />
-        <script type="module" defer src="https://static.rocket.new/rocket-shot.js?v=0.0.2" /></body>
+        </body>
     </html>
   );
 }
